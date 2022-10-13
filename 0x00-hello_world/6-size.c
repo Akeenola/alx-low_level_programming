553911724c16dd78e7fe78fea91508dc7ffba6e5
akeenola <akinola.olabisi@gmail.com>
2022-10-13 15:52:01 -0700

    exe c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 #include<stdio.h>
 int main(void)
 {
-	printf("Size of char:%lu", sizeof(char));
-	printf("Size of an int:%lu", sizeof(int));
-	printf("Size of a long int:%lu", sizeof(long int));
-	printf("Size of long long int:%lu", sizeof(long long int));
-	printf("Size of a float:%lu", sizeof(float));
+	printf("Size of char:%lu\n", sizeof(char));
+	printf("Size of an int:%lu\n", sizeof(int));
+	printf("Size of a long int:%lu\n", sizeof(long int));
+	printf("Size of long long int:%lu\n", sizeof(long long int));
+	printf("Size of a float:%lu\n", sizeof(float));
 }
