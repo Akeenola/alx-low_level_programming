f10b9de245c37c34db56221057de517c5d0c6dec
akeenola <akinola.olabisi@gmail.com>
2022-10-13 15:03:23 -0700

    Add the first c file without gccc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+*Main-entry
+*Description: 'the program's description'
+*Return: 0 success
+*/
+#include<std.io>
+int main(void)
+{
+  printf("with proper grammar\, but the outcome is a piece of art\,");
+  return (0);
+}
