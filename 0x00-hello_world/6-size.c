11e7f9b2933f5115cb14c760fb21e5ad7f00a24c
akeenola <akinola.olabisi@gmail.com>
2022-10-13 16:52:16 -0700

    add exe files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of char:\t%lu\tbyte(s)\n", sizeof(char));
 	printf("Size of an int:\t%lu\tbyte(s)\n", sizeof(int));
 	printf("Size of a long int:\t%lu\tbyte(s)\n", sizeof(long int));
-	printf("Size of long long int:\t%lu\tbyte(s)\n", sizeof(long long int);
+	printf("Size of long long int:\t%lu\tbyte(s)\n", sizeof(long long int));
 	printf("Size of a float:\t%lu\tbyte(s)\n", sizeof(float));
 }
