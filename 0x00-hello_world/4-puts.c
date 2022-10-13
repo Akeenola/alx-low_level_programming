1cb66c3180afdee338610b5c57d6b93b55e5905b
akeenola <akinola.olabisi@gmail.com>
2022-10-13 14:32:27 -0700

    Add the first c file with extension:

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-/** program to 
-* print wit puts
-* and end with 0
-*main file included
+/*
+*main-entry
+*main-entry
+*return 0
 */
+#include<stdio.h>
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle \n");
-  return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
