32d8a14a3b2288ec97fe22e251b10720b63fac3c
akeenola <akinola.olabisi@gmail.com>
2022-10-13 13:31:46 -0700

    Create a  c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/** program to 
+* print wit puts
+* and end with 0
+*main file included
+*/
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle \n");
+  return (0);
+}
