90b99cd67f5ff04fd5c1df8b585e858a5e0a3646
akeenola <akinola.olabisi@gmail.com>
2022-10-13 15:45:14 -0700

    exe c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 *Description: 'the program's description'
 *Return: 0 success
 */
-#include<std.io>
+#include<stdio.h>
 int main(void)
 {
-  printf("with proper grammar\, but the outcome is a piece of art\,");
+  printf("with proper grammar, but the outcome is a piece of art,");
   return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+*main-entry point
+*Description:'the program's description'
+*Return:0 successul
+*/
+#include<stdio.h>
+int main(void)
+{
+	printf("Size of char:%lu", sizeof(char));
+	printf("Size of an int:%lu", sizeof(int));
+	printf("Size of a long int:%lu", sizeof(long int));
+	printf("Size of long long int:%lu", sizeof(long long int));
+	printf("Size of a float:%lu", sizeof(float));
+}
