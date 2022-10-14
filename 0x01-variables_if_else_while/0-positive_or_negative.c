f64265715298d563a0ab49c9b259942ca9978ad2
akeenola <akinola.olabisi@gmail.com>
2022-10-14 11:30:49 -0700

    Task on iff then using betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,13 +6,13 @@
 #include<stdlib.h>
 int	main(void)
 {
-int	num;
-num	=	rand();
-if	(num	>	0)
-    printf("%d is  positive",	num);
-else	if	(num	==	0)
-    printf("%d is zero",	num);
+int	n;
+n	=	rand()%99;
+if	(n	>	0)
+    printf("%d is  positive",	n);
+else	if	(n	==	0)
+    printf("%d is zero",	n);
 else
-    printf("%d is negative",	num);
+    printf("%d is negative",	n);
  return	(0);
 }
