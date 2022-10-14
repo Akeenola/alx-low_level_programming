512f1389ab790f533d4c50b003c19ee18a01eff0
akeenola <akinola.olabisi@gmail.com>
2022-10-13 20:13:38 -0700

    Add wite without print in c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 *main - Entry point
 *Return: Always 0 (Success)
 */
-#include<stdio.h>
+#include<unistd.h>
 int	main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 50);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 return (1);
 }
