d4cbebe4ad5eb66f47c1bfbed6d328ccc15a7f44
akeenola <akinola.olabisi@gmail.com>
2022-10-13 21:58:33 -0700

    task 3 on redirection s done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
 #include<stdio.h>
 int	main(void)
 {
-	int p;
-	long int q;
-	long long int r;
+	int	p;
+	long	int	q;
+	long	long 	int	r;
 	char s;
 	float t;
-	printf("Size of a char: %lu byte(s)\n",	(unsigned long)sizeof(s));
-	printf("Size of an int: %lu byte(s)\n",	(unsigned long)sizeof(p));
-	printf("Size of a long int: %lu byte(s)\n",	(unsigned long)sizeof(q));
-	printf("Size of a long long int: %lu byte(s)\n",	(unsigned long)sizeof(r));
-	printf("Size of a float: %lu byte(s)\n",	(unsigned long)sizeof(t));
-	return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned	long)	sizeof(s));
+	printf("Size of an int: %lu byte(s)\n", (unsigned	long)	sizeof(p));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned	long)	sizeof(q));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned	long)	sizeof(r));
+	printf("Size of a float: %lu byte(s)\n", (unsigned	long)	sizeof(t));
+	return	(0);
 }
