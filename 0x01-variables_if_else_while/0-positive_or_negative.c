a24deb76e97ac3c2ddd51d1821389e86427a6393
akeenola <akinola.olabisi@gmail.com>
2022-10-16 13:30:27 -0700

    taskk 0 on rand()

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int	n;
 srand(time(0));
 n	=	rand() -   RAND_MAX / 2;
 if	(n	>	0)
-    printf("%i is  positive\n",	n%100);
+    printf("%i is positive\n",	n%100);
 else	if	(n	==	0)
     printf("%i is zero\n",	n);
  else	if	(n	<	0)
