1a90fb3a5f991573cee6943b2ca692dae47f13a6
akeenola <akinola.olabisi@gmail.com>
2022-10-16 18:00:48 -0700

    task 8 complete

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,20 +4,20 @@
  *Return: 0 when successful
  */
 #include<stdio.h>
-int main(void)
+int	main(void)
 {
-  char l;
-  l='0';
-  while(l<='9')
-    {
-      putchar(l);
-      l++;
-    }
-  l='a';
-  while(l<='f')
-    {
-      putchar(l);
-      l++;
-    }
-  return (0);
+	char l;
+	l	=	'0';
+	while	(l	<=	'9')
+	{
+		putchar(l);
+		l++;
+	}
+	l	=	'a';
+	while	(l	<=	'f')
+	{
+		putchar(l);
+		l++;
+	}
+	return	(0);
 }
