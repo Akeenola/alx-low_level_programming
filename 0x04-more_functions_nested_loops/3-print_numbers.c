69ca3699b8b9ac33e9319b8d7ed9c911aac42518
akeenola <akinola.olabisi@gmail.com>
2022-10-20 20:37:50 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,14 @@
 #include"main.h"
+/**
+ * print_numbers p print usinff -putchar
+ */
 void print_numbers(void)
 {
-  char c;
-  c='0';
-  while(c<='9')
-    {
-  _putchar(c);
-  c++;
-
-    }
+	char	c;
+	c	=	'0';
+	while	(c	<=	'9')
+	{
+		_putchar(c);
+		c++;
+	}
 }
