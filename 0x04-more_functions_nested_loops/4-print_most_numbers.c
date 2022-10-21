cc2d1b822b0c0e973d6d630e9f43ff89821dcc86
akeenola <akinola.olabisi@gmail.com>
2022-10-20 21:32:56 -0700

    print withoue  and 4:

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include"main.h"
+void	print_most_numbers(void)
+{
+	int	c;
+	c	=	0;
+	while	(c	<=	9)
+	{
+		if	(c	!=	2	&&	c	!=	4)
+		{
+			putchar(c	+	'0');
+		}
+		c++;
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _isdigit(int c);
 void _putchar(char a); 
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif /* MAIN_H */
