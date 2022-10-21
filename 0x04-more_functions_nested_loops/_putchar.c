c852dfb62db5651f6b47e53082d40f73564e0923
akeenola <akinola.olabisi@gmail.com>
2022-10-20 18:55:34 -0700

    is digit task 0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,27 @@
+/**
+
+ *_putchar-ptint characters
+
+ *
+
+ *a-received arg
+
+ *
+
+ *char-argument type
+
+ */
+
+#include<stdio.h>
+
+#include"main.h"
+
+void _putchar(char);
+
+void _putchar(char a)
+  
+{
+  
+  putchar(a);
+  
+}
