a820f68cbb0af7a434ec9f8310244d7d9e35984e
akeenola <akinola.olabisi@gmail.com>
2022-10-24 19:28:53 -0700

    task on swap complete

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * swap_int - swap function
+ *
+ * @a : parameter a
+ *
+ * @b : parameter b
+ */
+void	swap_int(int	*a,	int	*b)
+{
+	int	tmp	=      *a;
+	*a	=	*b;
+	*b	=	tmp;
+}
