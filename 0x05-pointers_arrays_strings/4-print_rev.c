bb458dd71091e7edffe2cb01ba6a057ffe35a09f
akeenola <akinola.olabisi@gmail.com>
2022-10-24 21:10:35 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include<string.h>
+/**
+ * print_rev - function name
+ *
+ *@s : parameter
+ */
+void	print_rev(char	*s)
+{
+	int	n	=	strlen(s);
+	int	i;
+	for(i	=	(n-1);	i	>=	0;	i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
