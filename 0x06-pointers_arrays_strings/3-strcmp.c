cb24f27eebdcee63981526575557ded05a733ddf
akeenola <akinola.olabisi@gmail.com>
2022-10-27 23:14:16 -0700

    task 3 readded

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,15 @@
 #include"main.h"
 #include<string.h>
-int _strcmp(char *s1, char *s2)
+/**
+ * _strcmp - function name
+ *
+ * Return: always @strcmp(s1,s2)
+ *
+ *@s1:  parameter
+ *
+ *@s2: parameter
+ */
+int	_strcmp(char	*s1,	char	*s2)
 {
-  return strcmp(s1,s2);
+	return	(strcmp(s1,	s2));
 }
