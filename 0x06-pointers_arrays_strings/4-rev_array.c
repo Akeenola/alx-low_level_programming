2f7b682969b6e83bb238d59f04c891753fe8a456
akeenola <akinola.olabisi@gmail.com>
2022-10-27 22:59:45 -0700

    task 4 readded

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,7 +6,6 @@
  * @a: parameter
  *
  * @n: parameter
-
  */
 void	reverse_array(int	*a,	int	n)
 {
@@ -14,7 +13,7 @@ void	reverse_array(int	*a,	int	n)
 	int	tmp;
 	for	(i	=	0;	i	<	n	/	2;	i++)
 	{
-		tmp=a[i];
+		tmp	=	a[i];
 		a[i]	=	a[n	-	i	-	1];
 		a[n	-	i	-	1]	=	tmp;
 	}
