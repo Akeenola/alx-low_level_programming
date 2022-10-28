59d13ec84e696be0e4eaa8fc47a6a48a1552df11
akeenola <akinola.olabisi@gmail.com>
2022-10-27 23:03:30 -0700

    task 5 readded

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,6 @@ char	*string_toupper(char	*tmp)
 	int	len;
 	int	i;
 	len	=	strlen(tmp);
-	printf("length of string %d\n",	len);
 	for	(i	=	0;	i	<	len;	i++)
 	{
 		if	(tmp[i] 	>=	'a'	&&	tmp[i]	<=	'z')
