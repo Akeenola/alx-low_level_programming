f6ddecacc323fcb69ae285a3fef024ec6fc40588
akeenola <akinola.olabisi@gmail.com>
2022-10-28 18:44:59 -0700

    add task 9 again

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,53 +1,45 @@
 #include"main.h"
 #include<string.h>
-void print_number(int n)
+/**
+ * print_number - function name
+ *
+ *@n : parameter
+ */
+void	print_number(int	n)
 {
-  int i;
-  int j;
-  int k;
-  char str[10];
-  k =0;
-  if(n==0)
-    _putchar(n+'0');
-  else if(n>=1)
-    {
-  while(n)
-    {
-      i=n/10;
-      j=n%10;
-      n=i;
-      str[k]=j+'0';    
-      /*      _putchar(j+'0');*/
-      k++;
-    }
-  for(k=9;k>=0;k--)
-    _putchar(str[k]);
-    }
-  else
-    {
-      _putchar('-');
-      n=-1*n;
-      while(n)
-	
+	int	i;
+	int	j;
+	int	k;
+	char	str[10];
+	k	=	0;
+	if	(n	==	0)
+		_putchar(n	+	'0');
+	else	if	(n	>=	1)
 	{
-	  
-	  i=n/10;
-	  
-	  j=n%10;
-	  
-	  n=i;
-	  
-	  str[k]=j+'0';
-	  
-	  /*      _putchar(j+'0');*/
-	  
-	  k++;
-	  
+		while	(n)
+		{
+			i	=	n	/	10;
+			j	=	n	%	10;
+			n	=	i;
+			str[k]	=	j	+	'0';
+			k++;
+		}
+		for	(k	=	9;	k	>=	0;	k--)
+			_putchar(str[k]);
 	}
-      
-      for(k=9;k>=0;k--)
-	
-	_putchar(str[k]);
-
-    }
-    }
+	else
+	{
+		_putchar('-');
+				n	=	(-1)	*	n;
+		while	(n)
+		{
+			i	=	n	/	10;
+			j	=	n	%	10;
+			n	=	i;
+			str[k]	=	j	+	'0';
+			k++;
+		}
+		for	(k	=	9;	k	>=	0;	k--)
+			_putchar(str[k]);
+	}
+}
