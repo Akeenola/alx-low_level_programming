ce771ee0f1b00e23515903e79fad777a263d24de
akeenola <akinola.olabisi@gmail.com>
2023-02-16 11:15:39 -0800

    task 4 complete

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,6 @@
 #include<stdio.h>
 int	main(void)
 {
-<<<<<<< HEAD
-	puts("Programming is like building a multilingual puzzle");
-=======
 	puts("\"Programming is like building a multilingual puzzle");
->>>>>>> 22ea310419b93c7845976a9a676cf4cf8bcd3877
 	return (0);
 }
