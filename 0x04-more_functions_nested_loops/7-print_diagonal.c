bc3bdbdcd95b449bb7315a616f522a6bd6772f12
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-02-28 03:49:34 +0100

    Task 7 edited

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,28 @@
-#include"main.h"
+#include "main.h"
 /**
- *print_diagonal - method
- *@n - parameter
+ *print_diagonal - prints a line n chars long.
+ *@n: number of \.
+ *
+ *Return: void.
  */
-void	print_diagonal(int n)
+void print_diagonal(int n)
 {
-	int	i;
-	int	j;
-	j	=	0;
-	if	(n	<=	0)
-		_putchar('\n');
-	for	(i	=	1;	i	<=	n;	i++)
+	int i, space;
+
+	if (n <= 0)
 	{
-	  j	=	i;
-		while	(j	>=	1)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			putchar(' ');
-			j--;
+			for (space = 0; space < i; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
 		}
-	_putchar('\\');
-	_putchar('\n');
-}
+	}
 }
