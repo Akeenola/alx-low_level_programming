ec84f08d9e8548f8a85c3ac829e0c18f58ebd187
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-02-28 03:50:52 +0100

    Task 6 corrected

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,25 @@
-#include"main.h"
+#include "main.h"
+
 /**
- *print_line
- *@n - parameter
+ *print_line - prints a line n chars long.
+ *@n: length of line.
+ *
+ *Return: void.
  */
-void	print_line(int	n)
+void print_line(int n)
 {
-	int	i;
-	if	(n	<=	0)
+	int i;
+
+	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-	for	(i	=	0;	i	<	n;	i++)
-		_putchar('_');
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 	_putchar('\n');
 	}
 }
