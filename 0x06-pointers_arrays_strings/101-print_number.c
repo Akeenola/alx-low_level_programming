293e0b669a28285f072a530ed85ede560d9db10c
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-03-02 21:20:13 +0100

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,45 +1,26 @@
-#include"main.h"
-#include<string.h>
+include "holberton.h"
+
 /**
- * print_number - function name
- *
- *@n : parameter
- */
-void	print_number(int	n)
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
+ **/
+
+void print_number(int n)
 {
-	int	i;
-	int	j;
-	int	k;
-	char	str[10];
-	k	=	0;
-	if	(n	==	0)
-		_putchar(n	+	'0');
-	else	if	(n	>=	1)
-	{
-		while	(n)
-		{
-			i	=	n	/	10;
-			j	=	n	%	10;
-			n	=	i;
-			str[k]	=	j	+	'0';
-			k++;
-		}
-		for	(k	=	9;	k	>=	0;	k--)
-			_putchar(str[k]);
-	}
-	else
-	{
-		_putchar('-');
-				n	=	(-1)	*	n;
-		while	(n)
-		{
-			i	=	n	/	10;
-			j	=	n	%	10;
-			n	=	i;
-			str[k]	=	j	+	'0';
-			k++;
-		}
-		for	(k	=	9;	k	>=	0;	k--)
-			_putchar(str[k]);
-	}
+unsigned int n1;
+
+n1 = n;
+
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
+
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
