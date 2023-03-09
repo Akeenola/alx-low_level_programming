5b14a14594fd89df51c29dfb4269fc919050f3f4
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-03-09 04:30:04 +0100

    Update _putchar.c

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,1 +1,15 @@
-gf
+/**
+ *_putchar-ptint characters
+ *
+ *a-received arg
+ *
+ *char-argument type
+ */
+#include<stdio.h>
+#include"main.h"
+void _putchar(char);
+void _putchar(char a)
+{
+  putchar(a);
+}
+
