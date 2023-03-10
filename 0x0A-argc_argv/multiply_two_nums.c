b2d08f4c762057a59a49f3f8e118c5cfa1e9104f
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-03-10 18:12:00 +0100

    Multiply function created

---
+++ 0x0A-argc_argv/multiply_two_nums.c
@@ -0,0 +1,14 @@
+/**
+ * multiply_two_nums - Multiplies two numbers
+ *
+ * @num1: A number
+ *
+ * @num2: A number
+ *
+ * Return: Result of num1 * num2
+ */
+
+int multiply_two_nums(int num1, int num2)
+{
+	return (num1 * num2);
+}
