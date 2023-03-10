e30ab52ddfeb1e40d78b11b08aa15ddd6b7eb038
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-03-10 17:44:59 +0100

    Task 1 corrected

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives
- * @argc: number of command line arguments count
- * @argv: array of command line arguments vector
+ * main - prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: array of the command line arguments
  *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	while (argc--)
-		printf("%s\n", *argv++);
+	int i;
+
+	(void)argv;
+	i = 0;
+	while (i < argc - 1)
+		i++;
+	printf("%d\n", i);
 	return (0);
 }
