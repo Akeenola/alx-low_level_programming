a8cd712a147e1fe542ff87cfcc3f88d2c14fa5bc
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-03-22 03:10:20 +0100

    Task 0 edited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
   *struct dog - structure of a dog.
@@ -13,6 +13,12 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 
 #endif /* DOG_H */
