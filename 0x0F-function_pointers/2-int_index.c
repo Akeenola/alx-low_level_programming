dfa1358eb67c80bdfa2fb9aa02ac79816b989372
akeenola <akinola.olabisi@gmail.com>
2023-03-22 21:45:03 -0700

    task 2 dine

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,9 @@
+/**
+  *int_index - searches for an integer
+  *@array: array of integers
+  *@size: number of elements in array
+  *@cmp: pointer to function used to compare values
+  *
+  *Return: index of first element.
+  *-1 if no element is found or siz <= 0.
+  */
