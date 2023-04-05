b0aff7f02a38cd1036b0e3b84a9d6877ad421612
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-04-05 04:04:41 +0100

    Task 0 complete

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num_of_nodes = 0;
+	size_t m = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num_of_nodes++;
+		++;
 	}
-	return (num_of_nodes);
+	return (m);
 }
